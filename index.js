2ba4b4a6c022a490e46653e5de9c0087bf64874e
Richard <rgactr@gmail.com>
2019-09-24 10:50:44 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const axios = require("axios"); // Http Client
+const cors = require("cors"); // Cross Origin Resourse Sharing ( to make requests to other websites)
+
+// Middleware
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+  });
+}
+// Start server...
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+// Routes
+app.get("/api", (req, res) => {
+  axios
+    .get("https://api.github.com/users/reedbarger")
+    .then(response => {
+      console.log("WORKING");
+      res.json({ user: "Richard is the Greatest!" });
+    })
+    .catch(err => console.log("Shit", err));
+});
